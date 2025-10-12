33d7488bc50409a991d42ebb75449058319b9270
lgrison0311 <leoleof40@gmail.com>
2025-10-12 23:56:36 +0200

    Delete start.ps1

--- start.ps1
+++
@@ -1,7 +0,0 @@
-
-# Start the Valorant 5v5 Discord bot
-Set-StrictMode -Version Latest
-$here = Split-Path -Parent $MyInvocation.MyCommand.Path
-Set-Location $here
-. .\.venv\Scripts\Activate.ps1
-python bot.py
